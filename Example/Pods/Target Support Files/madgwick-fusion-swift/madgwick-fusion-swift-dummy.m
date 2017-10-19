acd2483669af633f849d032dbf0e3d35334f529c
Adam Leitgeb <leitgeb.a@icloud.com>
2017-10-19 19:48:56 +0200

    initial pod setup complete

---
+++ Example/Pods/Target Support Files/madgwick-fusion-swift/madgwick-fusion-swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_madgwick_fusion_swift : NSObject
+@end
+@implementation PodsDummy_madgwick_fusion_swift
+@end
---
+++ Example/Pods/Target Support Files/madgwick-fusion-swift/madgwick-fusion-swift-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double madgwick_fusion_swiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char madgwick_fusion_swiftVersionString[];
+
